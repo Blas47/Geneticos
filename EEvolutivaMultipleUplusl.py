eef1678cbf27f692e1ea3786e0e62bc857e35407
Blas47 <61062928+Blas47@users.noreply.github.com>
2021-12-01 13:16:54 +0100

    Fuera archivos de python

--- EEvolutivaMultipleUplusl.py
+++
@@ -1,9 +0,0 @@
-def runProcedimientoUplusl(poblacion):
-    print("Corriendo Algortimo: u+l...")
-    #devuelve la nueva poblacion una vez ejecutado una generacion
-    return poblacion
-
-
-
-if __name__ == "__main__":
-    print("Run the file desafioFinal!")
