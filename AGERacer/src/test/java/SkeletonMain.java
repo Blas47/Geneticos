6a7134120db0b300c5e4a3a8955c9b5159546922
Blas47 <61062928+Blas47@users.noreply.github.com>
2021-12-11 17:27:15 +0100

    pruebas

--- AGERacer/src/test/java/SkeletonMain.java
+++ AGERacer/src/test/java/SkeletonMain.java
@@ -94,7 +94,7 @@ public class SkeletonMain {
         }catch(Exception e) {}
 
         // Veces que queremos correr los experimentos para evitar mínimos locales.
-        int nVeces = 1;
+        int nVeces = 5;
 
         // Peor valor de fit que se puede obtener.
         double bestFit = 1000;
